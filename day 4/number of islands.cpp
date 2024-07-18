10dfabb53a840f06f63a13c8fdff01f6bfb7ac9a
Moutama Rakshit <155507095+moutamarakshit@users.noreply.github.com>
2024-07-18 11:50:41 +0530

    Update number of islands.cpp

--- day 4/number of islands.cpp
+++ day 4/number of islands.cpp
@@ -1,14 +1,20 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
 class Solution {
-public:
-    void dfs(int i, int j, vector<vector<int>>&vis,vector<vector<char>>& grid,int m, int n){
+  public:
+    // Function to find the number of islands.
+    void dfs(int i,int j, vector<vector<int>>&vis, vector<vector<char>>& grid, int n, int m){
         vis[i][j]=1;
-        int drow[]={0,0,1,-1};
-        int dcol[]={1,-1,0,0};
-        for(int k=0;k<4;k++){
+        int drow[]={0,0,1,-1,1,1,-1,-1};
+        int dcol[]={1,-1,0,0,1,-1,-1,1};
+        for(int k=0;k<8;k++){
             int nrow=i+drow[k];
             int ncol=j+dcol[k];
             if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1')
-            dfs(nrow,ncol,vis,grid,m,n);
+            dfs(nrow,ncol,vis,grid,n,m);
         }
     }
     int numIslands(vector<vector<char>>& grid) {
@@ -20,10 +26,33 @@ public:
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1' && vis[i][j]==0){
                     count++;
-                    dfs(i,j,vis,grid,m,n);
+                    dfs(i,j,vis,grid,n,m);
+                
                 }
             }
         }
         return count;
     }
 };
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
