81647274385c5fa610a4ec556749e57187b26a13
Moutama Rakshit <155507095+moutamarakshit@users.noreply.github.com>
2024-07-23 00:01:22 +0530

    Create repeated character.cpp
    added the code for "repeated character" problem

---
+++ day 9/repeated character.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    char firstRep (string s)
+    {
+        unordered_map<char,int>mpp;
+        for(char c:s){
+            mpp[c]++;
+        }
+        for(char c:s){
+        if(mpp[c]>1)
+        return c;
+        }
+        return '#';
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        char res = ob.firstRep (s);
+        if (res == '#') cout << -1 << endl;
+        else cout << res << endl;
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
