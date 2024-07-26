59132e9cc42f9ff6da75011996d266652a9b73b4
Moutama Rakshit <155507095+moutamarakshit@users.noreply.github.com>
2024-07-26 11:16:05 +0530

    Create chocolate distribution problem.cpp
    added the code for "chocolate distribution" problem

---
+++ day 12/chocolate distribution problem.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        if(m>n) return -1;
+    sort(a.begin(),a.end());
+    long long mini=LLONG_MAX;
+    for(int i=0;i<=n-m;i++){
+        long long diff=a[i+m-1]-a[i];
+        mini=min(diff,mini);
+    }
+    return mini;
+    }   
+};
+
+//{ Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
